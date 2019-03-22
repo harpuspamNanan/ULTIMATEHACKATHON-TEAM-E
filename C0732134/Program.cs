846b0011a760933ef110de016f85f8c1471a2f19
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-22 17:44:20 -0400

    Strings

--- C0732134/Program.cs
+++ C0732134/Program.cs
@@ -11,6 +11,10 @@ namespace C0732134
         static void Main(string[] args)
         {
 
+            string[] names = new string[16] {"Harpuspam", "Sidharath", "Harpreet", "Shingharjeet", "Bableen", "Harman", "Harshil", "Nirmal", "Tirth", "Akash", "Lionel Messi", "Sergio Busquests", "Gerard Pique", "Mohammad Salah", "Wayne Rooney", "Bruce Wayne"};
+            string[] letters = new string[4] { "A", "B", "C", "D" };
+
+
         }
     }
 }
