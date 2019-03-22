ae9db8f424a563d119af33b7ec7dce0d1af8eb4f
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-22 18:00:04 -0400

    done.... Maybe

--- C0732134/Program.cs
+++ C0732134/Program.cs
@@ -10,11 +10,38 @@ namespace C0732134
     {
         static void Main(string[] args)
         {
-
-            string[] names = new string[16] {"Harpuspam", "Sidharath", "Harpreet", "Shingharjeet", "Bableen", "Harman", "Harshil", "Nirmal", "Tirth", "Akash", "Lionel Messi", "Sergio Busquests", "Gerard Pique", "Mohammad Salah", "Wayne Rooney", "Bruce Wayne"};
+            int t = 0;
             string[] letters = new string[4] { "A", "B", "C", "D" };
+            string[] names = new string[16] {"Harpuspam", "Sidharath", "Harpreet", "Shingharjeet", "Bableen", "Harman", "Harshil", "Nirmal", "Tirth", "Akash", "Lionel Messi", "Sergio Busquests", "Gerard Pique", "Mohammad Salah", "Wayne Rooney", "Bruce Wayne"};
 
 
+            for (int j = 0; j <= 3; j++)
+            {
+                names[j] = names[j] + " " + letters[t];
+            }
+            t++;
+            for (int k = 4; k <= 7; k++)
+            {
+                names[k] = names[k] + " " + letters[t];
+            }
+            t++;
+            for (int l = 8; l <= 11; l++)
+            {
+                names[l] = names[l] + " " + letters[t];
+            }
+            t++;
+            for (int m = 12; m <= 15; m++)
+            {
+                names[m] = names[m] + " " + letters[t];
+            }
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
